d0a8c6bbf575a110e55d55fb0a96b6afaccdd2bc
Ambadi7 <ambadikr24@gmail.com>
2025-01-09 11:37:53 +0530

    final3

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Contact Details
+# This project is a dynamic and responsive web application built with React. It showcases how to efficiently pass data between components using props and iterate over data using JavaScript’s .map() method to render dynamic content. The application is designed to be modular, reusable, and scalable, demonstrating core React concepts like data flow and component rendering.
+# Key Features:
+## Props: Data is passed from parent components to child components using props, which allows for a flexible and maintainable architecture.
+## Map: The .map() method is used to dynamically render a list of items based on data passed to the components, improving the user experience with a clean and functional interface.
+## Reusable Components: Components are designed to be reusable and can easily be expanded or modified.
 ![contactdetails](./ss1.png)
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,9 +8,9 @@ const Contact = (props) => {
                 <img src={props.image} alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
                 <div className="flex flex-col justify-between p-6 space-y-8">
                     <div className="space-y-2">
-                        <h2 className="text-3xl font-semibold tracking-wide">Name : {props.name}</h2>
-                        <h2 className="text-3xl font-semibold tracking-wide">ph no :{props.phoneNo}</h2>
-                        <h2 className="text-3xl font-semibold tracking-wide">email : {props.email}</h2>
+                        <h2 className="text-xl md:text-3xl font-semibold tracking-wide">Name : {props.name}</h2>
+                        <h2 className="text-xl md:text-3xl font-semibold tracking-wide">ph no :{props.phoneNo}</h2>
+                        <h2 className="text-xl md:text-3xl font-semibold tracking-wide">email : {props.email}</h2>
                         
                     </div>
                 </div>
